1c7f7645c67a2626abdba0d5add98e2e89a565b8
code258 <81000546+code258@users.noreply.github.com>
2021-05-01 13:39:41 +0900

    Add files via upload

--- login_info.py
+++ login_info.py
@@ -1,36 +1,24 @@
-'''
-def login_check(input_id, input_pwd):
-    f = open('temp.txt', 'r')
-    lines = f.readlines()
-    flag = False
-    for line in lines:
-        temp = line.split('\n')[0]
-        id, pwd = temp.split(',')
-        if id == input_id and pwd == input_pwd:
-            flag = True
-            break
-    return flag
-'''
-
-'''
-temp = open('temp.txt', 'r')
-lines = temp.readlines()
-print(lines)
-temp.close()
-'''
-
-'''
-f = open('temp.txt', 'a')
-f.write("\naaa,bbb")
-'''
-temp = open('temp.txt', 'r')
-lines = temp.readlines()
-
+f = open("temp.txt", 'r')
+lines = f.readlines()
+total_id = []
 for line in lines:
-    temp = line.split('\n')[0]
-    id, pwd = temp.split(',')
-    print(id)
-
-account = open('temp.txt', 'a')
+    id, pwd = line.split(",")
+    total_id.append(id)
+f.close()
+print(total_id)
+f = open("temp.txt", 'a')
 produce_id = input("Enter the your produce id : ")
-account.write("\n" + produce_id)
+produce_pwd = input("Enter the your produce pwd : ")
+if produce_id not in total_id:
+    f.write("\n" + produce_id)
+    f.write("," + produce_pwd)
+
+else:
+    while produce_id in total_id:
+        print("Your ID is duplicated.")
+        produce_id = input("Enter the your produce id : ")
+        produce_pwd = input("Enter the your produce pwd : ")
+
+    if produce_id not in total_id:
+        f.write("\n" + produce_id)
+        f.write("," + produce_pwd)
