88daedbb647bf7d8e82484389b428e8abc6a4a55
code258 <81000546+code258@users.noreply.github.com>
2021-05-29 13:37:18 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,79 @@
-def push(stack, data, max_length):
-    if len(stack) < max_length:
-        stack.append(data)
-    else:
-        print("over flow")
+'''
+import random
 
-    return stack
+a = []
 
-def pop(stack):
-    if len(stack) > 0:
-        del(stack[4 - choice_pop])
-    else:
-        print("under flow")
+for k in range(0, 10):
+    b = random.randrange(10)
+    a.append(b)
 
-max_length = 3
-stack = []
-choice_pop = 0
-while True:
+print(a)
 
-    que = input("Enter push or pop : ")
+for i in range(10):
+    for i in range(0, 9):
+        if a[i] > a[i + 1]:
+            a[i], a[i + 1] = a[i + 1], a[i]
 
-    if que == "push":
-        data = int(input("Enter the number : "))
-        stack = push(stack, data, max_length)
+print(a)
+'''
 
-    elif que == "pop":
-        stack = pop(stack)
-        choice_pop += 1
+'''
+import random
+def make_list():
+    a = []
+    for k in range(0, 10):
+        b = random.randrange(10)
+        a.append(b)
+    return a
+
+a = make_list()
+print(a)
+
+def change_list():
+    for i in range(10):
+        for i in range(0, 9):
+            if a[i] > a[i + 1]:
+                a[i], a[i + 1] = a[i + 1], a[i]
+
+    return a
+
+a = change_list()
+print(a)
+'''
+import random
+
+a = []
+
+for k in range(0, 10):
+    b = random.randrange(10)
+    a.append(b)
+
+print(a)
+c = 0
+
+for j in range(len(a)):
+    if c > a[j]:
+        c = a[j]
+print(c)
+
+
+'''
+for l in range(0, 10):
+    for j in range(0, 10):
+        if a[j] == c:
+            print(a[j])
+            break
+        if a[j] != c and j == 9:
+            c += 1
+'''
+
+'''
+1. 함수화를 먼저 시키고
+2. select sort
+
+4,2,1,3,100
+
+1,2,4,3,100
+
+1,2,3,4,100
+'''
