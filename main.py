b66400db8b570c88989b94ea00289247e9ae4a27
code258 <81000546+code258@users.noreply.github.com>
2022-01-29 13:39:08 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,206 +1,111 @@
-'''
-#71
-my_variable = ()
-'''
+import random
+import pygame
+import sys
 
-'''
-#72
-movie_rank = ('닥터스트레인저', '스플릿', '럭키')
-'''
+pygame.init()
 
-'''
-#73
-t = (1)
-'''
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 500
 
-'''
-#75
-t = 1, 2, 3, 4
-print(type(t))
-'''
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 128, 0)
+TRANSPARENT = (0, 0, 0, 0)
 
-'''
-#76
-t = ('a', 'b', 'c')
-t = ('A', 'B', 'C')
-print(t)
-'''
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Flappy Bird')
+clock = pygame.time.Clock()
+screen.fill(WHITE)
 
-'''
-#77
-interest = ('삼성전자', 'LG전자', 'SK')
-interest = list(interest)
-print(interest)
-'''
-
-'''
-#78
-interest = ['삼성전자', 'LG전자', 'SK']
-interest = tuple(interest)
-print(interest)
-'''
-
-'''
-#79
-temp = ('apple', 'banana', 'cake')
-a, b, c = temp
-print(a, b, c)
-'''
-
-'''
-#80
-a = []
-for i in range (1, 100):
-    if i % 2 == 0:
-        a.append(i)
-print(a)
-
-a = tuple(a)
-print(a)
-'''
-
-'''
-def make_tuple():
-    test = []
-    for i in range(1, 100):
-        if i % 2 == 0:
-            test.append(i)
-    tuple(test)
-    return test
-
-my_tuple = make_tuple()
-
-print(my_tuple)
-'''
-
-'''
-#81
-scores = [8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5, 7.8, 9.4]
-a, b, *c = scores
-c, a*, b = scores
-print(a)
-'''
-
-'''
-temp = {}
-temp = {'a':123, 123 : 'c', 'a' : 123123123123}
-temp['b'] = 123
-print(temp)
-'''
-
-'''
-#84
-temp = {}
-'''
-
-'''
-#85
-temp = {'a' : 1000, 'b' : 1200, 'c' : 1800}
-'''
-
-'''
-#86
-temp = {'a' : 1000, 'b' : 1200, 'c' : 1800}
-temp['d'] = 1200
-temp['e'] = 1500
-print(temp)
-'''
-
-'''
-#87
-temp = {'a' : 1000, 'b' : 1200, 'c' : 1800}
-print(temp['a'])
-'''
-
-'''
-#88
-temp = {'a' : 1000, 'b' : 1200, 'c' : 1800, 'd' : 1200, 'e' : 1500}
-temp['a'] = 1300
-print(temp)
-'''
-
-'''
-#89
-temp = {'a' : 1000, 'b' : 1200, 'c' : 1800, 'd' : 1200, 'e' : 1500}
-del temp['a']
-print(temp)
-'''
-
-'''
-#91
-inventory = {'a' : [300, 20], 'b' : [400, 3], 'c' : [250, 100]}
-print(inventory)
-'''
-
-'''
-#92
-inventory = {'a' : [300, 20], 'b' : [400, 3], 'c' : [250, 100]}
-print(inventory['a'][0])
-'''
-
-'''
-#93
-inventory = {'a' : [300, 20], 'b' : [400, 3], 'c' : [250, 100]}
-print(inventory['a'][1])
-'''
-
-'''
-#94
-inventory = {'a' : [300, 20], 'b' : [400, 3], 'c' : [250, 100]}
-inventory['d'] = [500, 7]
-print(inventory)
-'''
-
-'''
-#95
-temp = {'a' : 1200, 'b' : 1200, 'c' : 1800, 'd' : 1500, 'e' : 1000}
-print(list(temp.keys()))
-'''
-
-'''
-#96
-temp = {'a' : 1200, 'b' : 1200, 'c' : 1800, 'd' : 1500, 'e' : 1000}
-print(list(temp.values()))
-'''
-
-'''
-#97
-temp = {'a' : 1200, 'b' : 1200, 'c' : 1800, 'd' : 1500, 'e' : 1000}
-values = list(temp.values())
-print(values)
-total_cost = 0
-for value in values:
-    total_cost += value
-
-print("total_cost : {}".format(total_cost))
-'''
+background = pygame.image.load("sprites/zbackground.png")
+down = pygame.image.load("sprites/adown.png")
+up = pygame.image.load("sprites/aup.png")
+ax = SCREEN_WIDTH + 70
+pos_x = 0
+location = []
+interval = SCREEN_WIDTH / 3 + 100
+random1, random2 = 0, 0
+score = 0
 
 
-#99
-'''
-temp = ('a', 'b', 'c')
-test = (1, 2, 3)
+class FlappyBird(object):
+    def __init__(self):
+        self.image = pygame.image.load('sprites/flappy_bird.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (100, 100))
 
-output = zip(temp, test)
+    def draw(self, x, y):
+        screen.blit(self.image, (x, y))
 
-print(list(output))
-'''
 
-'''
-keys = {'apple', 'pear', 'peach'}
+def moveB(pos_x, background):
+    pos_x -= 3
+    if pos_x < -700:
+        pos_x = 0
+    screen.blit(background, [pos_x, 0])
+    screen.blit(background, [pos_x + 700, 0])
+    return pos_x
 
-yals = {300, 250, 400}
 
-output = zip(keys, yals)
-print(dict(output))
-'''
+def crashed():
+    font = pygame.font.SysFont("arial", 40)
+    text = font.render("You were crashed by the obstacle.", True, BLACK)
+    screen.blit(text, (75, 200))
+    pygame.quit()
+    sys.exit()
 
-'''
-#100
-date = ['09/05', '09/06', '09/07', '09/08', '09/09']
 
-close_price = [10500, 10300, 10100, 10000, 11000]
+random_y = random.randrange(-400, -100)
+for i in range(3):
+    location.append([ax, random_y])
+    ax = -75
 
-output = zip(date, close_price)
-print(dict(output))
-'''
+
+flappy_bird = FlappyBird()
+flappy_birds = []
+
+
+def game_loop():
+    x, y = 300, 200
+    speed = 1
+    check = 0
+    global pos_x
+    global interval
+    global score
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    flappy_bird.draw(x, y)
+                    speed = -3
+        if y < -200 or y > 500:
+            crashed()
+        y += speed
+        speed += 0.1
+        pos_x = moveB(pos_x, background)
+        for i in range(3):
+            if location[i][0] < -70:
+                random_y = random.randrange(-400, -100)
+                location[i] = [location[(i + 2) % 3][0] + interval, random_y]
+            location[i][0] -= 3
+            screen.blit(down, [location[i][0], location[i][1]])
+            screen.blit(up, [location[i][0], location[i][1] + 570])
+            if location[i][0] - 10 < x < location[i][0] + 10:
+                if y > location[i][1] + 495 or y < location[i][1] + 400:
+                    crashed()
+        flappy_bird.draw(x, y)
+        pygame.time.delay(10)
+        check += 1
+        if check == 120:
+            check = 0
+            score += 1
+        font = pygame.font.SysFont("arial", 25)
+        text = font.render("Score: {}".format(score), True, BLACK)
+        screen.blit(text, (3, 5))
+        pygame.display.update()
+        screen.fill(WHITE)
+        clock.tick(59)
+
+
+game_loop()
+        
